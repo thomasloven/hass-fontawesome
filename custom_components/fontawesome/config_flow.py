a8f05da043024b16c30017f05316c9bd6ce4f047
Thomas Lovén <thomasloven@gmail.com>
2024-09-23 13:35:01 +0000

    Fix #79

--- custom_components/fontawesome/config_flow.py
+++ custom_components/fontawesome/config_flow.py
@@ -4,10 +4,15 @@ from homeassistant import config_entries
 
 _LOGGER = logging.getLogger(__name__)
 
+DOMAIN = "fontawesome"
 
-@config_entries.HANDLERS.register("fontawesome")
+
+@config_entries.HANDLERS.register(DOMAIN)
 class FontawesomeConfigFlow(config_entries.ConfigFlow):
+
+    VERSION = 2
+
     async def async_step_user(self, user_input=None):
         if self._async_current_entries():
             return self.async_abort(reason="single_instance_allowed")
-        return self.async_create_entry(title="", data={})
+        return self.async_create_entry(title="Fontawesome Icons", data={})
