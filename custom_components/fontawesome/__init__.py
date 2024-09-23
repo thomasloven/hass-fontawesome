ed22bd76f9c95fbf327e26565376737e54a9e0d9
Thomas Lovén <thomasloven@gmail.com>
2024-09-23 12:41:13 +0000

    Update Fontawesome to 6.6.0

--- custom_components/fontawesome/__init__.py
+++ custom_components/fontawesome/__init__.py
@@ -1,5 +1,6 @@
 import logging
 
+from homeassistant.core import HomeAssistant
 from homeassistant.components.frontend import add_extra_js_url
 from homeassistant.components.http import StaticPathConfig
 from homeassistant.components.http.view import HomeAssistantView
@@ -13,14 +14,14 @@ LOGGER = logging.getLogger(__name__)
 
 DOMAIN = "fontawesome"
 
-DATA_EXTRA_MODULE_URL = 'frontend_extra_module_url'
-LOADER_URL = f'/{DOMAIN}/main.js'
-LOADER_PATH = f'custom_components/{DOMAIN}/main.js'
-ICONS_URL = f'/{DOMAIN}/icons'
-ICONLIST_URL = f'/{DOMAIN}/list'
-ICONS_PATH = f'custom_components/{DOMAIN}/data'
-CUSTOM_ICONS_URL = f'/{DOMAIN}/icons/pro'
-CUSTOM_ICONS_PATH = 'custom_icons'
+DATA_EXTRA_MODULE_URL = "frontend_extra_module_url"
+LOADER_URL = f"/{DOMAIN}/main.js"
+LOADER_PATH = f"custom_components/{DOMAIN}/main.js"
+ICONS_URL = f"/{DOMAIN}/icons"
+ICONLIST_URL = f"/{DOMAIN}/list"
+ICONS_PATH = f"custom_components/{DOMAIN}/data"
+CUSTOM_ICONS_URL = f"/{DOMAIN}/icons/pro"
+CUSTOM_ICONS_PATH = "custom_icons"
 
 
 CONFIG_SCHEMA = cv.empty_config_schema(DOMAIN)
@@ -33,33 +34,31 @@ class ListingView(HomeAssistantView):
     def __init__(self, url, iconpath, hass):
         self.url = url
         self.iconpath = iconpath
-        self.hass = hass
+        self.hass: HomeAssistant = hass
         self.name = "Icon Listing"
 
     async def get(self, request):
-        icons_list = await self.hass.async_add_executor_job(self.get_icons_list, self.iconpath)
+        icons_list = await self.hass.async_add_executor_job(
+            self.get_icons_list, self.iconpath
+        )
         return icons_list
 
     def get_icons_list(self, iconpath):
         icons = []
-        for (dirpath, dirnames, filenames) in walk(iconpath):
+        for dirpath, dirnames, filenames in walk(iconpath):
             icons.extend(
                 [
-                    {"name": path.join(dirpath[len(iconpath):].lstrip('/'), fn[:-4])}
-                    for fn in filenames if fn.endswith(".svg")
+                    {"name": path.join(dirpath[len(iconpath) :].lstrip("/"), fn[:-4])}
+                    for fn in filenames
+                    if fn.endswith(".svg")
                 ]
             )
         return json.dumps(icons)
 
-async def async_setup(hass, config):
+
+async def async_setup(hass: HomeAssistant, config):
     await hass.http.async_register_static_paths(
-        [
-            StaticPathConfig(
-                LOADER_URL,
-                hass.config.path(LOADER_PATH),
-                True
-            )
-        ]
+        [StaticPathConfig(LOADER_URL, hass.config.path(LOADER_PATH), True)]
     )
     add_extra_js_url(hass, LOADER_URL)
 
@@ -69,33 +68,23 @@ async def async_setup(hass, config):
                 StaticPathConfig(
                     ICONS_URL + "/" + iset,
                     hass.config.path(ICONS_PATH + "/" + iset),
-                    True
+                    True,
                 )
             ]
         )
         hass.http.register_view(
-                ListingView(
-                    ICONLIST_URL + "/" + iset,
-                    hass.config.path(ICONS_PATH + "/" + iset),
-                    hass
-                )
-            )
-    await hass.http.async_register_static_paths(
-        [
-            StaticPathConfig(
-                CUSTOM_ICONS_URL,
-                hass.config.path(CUSTOM_ICONS_PATH),
-                True
-            )
-        ]
-    )
-    hass.http.register_view(
             ListingView(
-                ICONLIST_URL + "/pro",
-                hass.config.path(CUSTOM_ICONS_PATH),
-                hass
+                ICONLIST_URL + "/" + iset,
+                hass.config.path(ICONS_PATH + "/" + iset),
+                hass,
             )
         )
+    await hass.http.async_register_static_paths(
+        [StaticPathConfig(CUSTOM_ICONS_URL, hass.config.path(CUSTOM_ICONS_PATH), True)]
+    )
+    hass.http.register_view(
+        ListingView(ICONLIST_URL + "/pro", hass.config.path(CUSTOM_ICONS_PATH), hass)
+    )
 
     return True
 
